399e8cda1ed84014a986e02b767efee29a16f0be
Andrei <andreibadoiu@ymail.com>
2021-04-17 17:23:42 +0100

    first commit

--- README.txt
+++ README.txt
@@ -1,5 +1,16 @@
-Python script that analyses news headline or body sentiment and returns the overall media sentiment of any given coin. 
+Fully functioning Binance Trading bot that Buys cryptocurrency based on Daily news sentiment from the top 100 crypto feeds.
 
-It can take multiple coins and keywords as input.
+Main Features:
+Pull and analyse the last headline from the top 100 crypto news sites
 
-Guide and possible use cases available at:
+Provide an overview on the most mentioned coin across all the headlines
+
+Analyse the sentiment of each headline and categorise the output by coin
+
+Place a Buy order if the compound sentiment is positive
+
+Configurable options
+
+Works with any number of cryptocurrencies
+
+For a complete step-by-step setup guide please see: https://www.cryptomaton.org/2021/04/17/how-to-code-a-binance-crypto-trading-bot-that-trades-based-on-daily-news-sentiment/
--- news-analysis.py
+++ news-analysis.py
@@ -1,129 +1,327 @@
-from datetime import datetime, date, timedelta
-import requests, json, re, os
+# import for environment variables and waiting
+import os, time
+
+# used to parse XML feeds
+import xml.etree.ElementTree as ET
+
+# get the XML feed and pass it to Element tree
+import requests
+
+# date modules that we'll most likely need
+from datetime import date, datetime, timedelta
+
+# used to grab the XML url list from a CSV file
+import csv
+
+# numpy for sums and means
+import numpy as np
+
+# nlp library to analyse sentiment
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+# needed for the binance API
+from binance.client import Client
+from binance.enums import *
+from binance.exceptions import BinanceAPIException, BinanceOrderException
+
+# used for binance websocket
+from binance.websockets import BinanceSocketManager
+from twisted.internet import reactor
+
+# used for executing the code
+from itertools import count
 
 
-sentiment_key = os.getenv('sentiment_key')
-websearch_key = os.getenv('websearch_key')
+# get binance key and secret from environment variables
+api_key = os.getenv('binance_api_stalkbot_testnet')
+api_secret = os.getenv('binance_secret_stalkbot_testnet')
 
-# user input variables
-# The key can be fed to the trading terminal for a position request.
-# Values are keyowrds to search the web for
-crypto_key_pairs = {"BTCUSD":"Bitcoin", "ETHUSD":"Ethereum", "LTCUSD":"Litecoin", "XRPUSD":"Ripple", "BATUSD":"BATUSD, basic attention token", "DSHUSD":"Dash Coin", "EOSUSD":"EOS", "ETCUSD":"ETC", "IOTUSD":"IOTA", "NEOUSD":"NEO", "OMGUSD":"OMISE Go", "TRXUSD":"Tron", "XLMUSD":"Stellar Lumens", "XMRUSD":"Monero", "ZECUSD":"Zcash"}
-#define from published date
-date_since = date.today() - timedelta(days=1)
+# Authenticate with the client
+client = Client(api_key, api_secret)
 
-#store inputs in different lists
-cryptocurrencies = []
-crypto_keywords = []
-
-#Storing keys and values in separate lists. Keys are used with the MT5 API while Values stand as values for the sentiment analysis
-for i in range(len(crypto_key_pairs)):
-    cryptocurrencies.append(list(crypto_key_pairs.keys())[i])
-    crypto_keywords.append(list(crypto_key_pairs.values())[i])
+# The API URL is manually changed in the library to work on the testnet
+client.API_URL = 'https://testnet.binance.vision/api'
 
 
-# Search the web for news unsing the websearch api, send a request for each crypto in cryprocurrencies
-def get_news_headlines():
-    '''Search the web for news headlines based the keywords in the global variable'''
-    news_output = {}
-
-    #TO DO - looping through keywords created odd looking dicts. Gotta loop through keys instead.
-    for crypto in crypto_keywords:
-
-        #create empty dicts in the news output
-        news_output["{0}".format(crypto)] = {'description': [], 'title': []}
-
-        #configure the fetch request and select date range. Increase date range by adjusting timedelta(days=1)
-        url = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI"
-        querystring = {"q":str(crypto),"pageNumber":"1","pageSize":"30","autoCorrect":"true","fromPublishedDate":date_since,"toPublishedDate":"null"}
-        headers = {
-            'x-rapidapi-key': websearch_key,
-            'x-rapidapi-host': "contextualwebsearch-websearch-v1.p.rapidapi.com"
-            }
-
-        #get the raw response
-        response = requests.request("GET", url, headers=headers, params=querystring)
-
-        # convert response to text format
-        result = json.loads(response.text)
-
-        #store each headline and description in the dicts above
-        for news in result['value']:
-            news_output[crypto]["description"].append(news['description'])
-            news_output[crypto]["title"].append(news['title'])
-
-    return news_output
 
 
-def analyze_headlines():
-    '''Analyse each headline pulled trhough the API for each crypto'''
-    news_output = get_news_headlines()
-
-    for crypto in crypto_keywords:
-
-        #empty list to store sentiment value
-        news_output[crypto]['sentiment'] = {'pos': [], 'mid': [], 'neg': []}
-
-        # analyse the description sentiment for each crypto news gathered
-        if len(news_output[crypto]['description']) > 0:
-            for title in news_output[crypto]['title']:
-
-                # remove all non alphanumeric characters from payload
-                titles = re.sub('[^A-Za-z0-9]+', ' ', title)
-
-                import http.client
-                conn = http.client.HTTPSConnection('text-sentiment.p.rapidapi.com')
-
-                #format and sent the request
-                payload = 'text='+titles
-                headers = {
-                    'content-type': 'application/x-www-form-urlencoded',
-                    'x-rapidapi-key': sentiment_key,
-                    'x-rapidapi-host': 'text-sentiment.p.rapidapi.com'
-                    }
-                conn.request("POST", "/analyze", payload, headers)
-
-                #get the response and format it
-                res = conn.getresponse()
-                data = res.read()
-                title_sentiment = json.loads(data)
-
-                #assign each positive, neutral and negative count to another list in the news output dict
-                if not isinstance(title_sentiment, int):
-                    if title_sentiment['pos'] == 1:
-                        news_output[crypto]['sentiment']['pos'].append(title_sentiment['pos'])
-                    elif title_sentiment['mid'] == 1:
-                        news_output[crypto]['sentiment']['mid'].append(title_sentiment['mid'])
-                    elif title_sentiment['neg'] == 1:
-                        news_output[crypto]['sentiment']['neg'].append(title_sentiment['neg'])
-                    else:
-                        print(f'Sentiment not found for {crypto}')
-
-    return news_output
+############################################
+#     USER INPUT VARIABLES LIVE BELOW      #
+# You may edit those to configure your bot #
+############################################
 
 
-def calc_sentiment():
-    '''Use the sentiment returned in the previous function to calculate %'''
-    news_output = analyze_headlines()
+# select what coins to look for as keywords in articles headlines
+# The key of each dict MUST be the symbol used for that coin on Binance
+# Use each list to define keywords separated by commas: 'XRP': ['ripple', 'xrp']
+# keywords are case sensitive
+keywords = {
+    'XRP': ['ripple', 'xrp', 'XRP', 'Ripple', 'RIPPLE'],
+    'BTC': ['BTC', 'bitcoin', 'Bitcoin', 'BITCOIN'],
+    'XLM': ['Stellar Lumens', 'XLM'],
+    'BCH': ['Bitcoin Cash', 'BCH'],
+    'ETH': ['ETH', 'Ethereum'],
+    'BNB' : ['BNB', 'Binance Coin'],
+    'LTC': ['LTC', 'Litecoin']
+    }
 
-    #re-assigned the sentiment list value to a single % calc of all values in each of the 3 lists
-    for crypto in crypto_keywords:
+# The Buy amount in the PAIRING symbol, by default USDT
+# 100 will for example buy the equivalent of 100 USDT in Bitcoin.
+QUANTITY = 100
 
-        #length of title list can't be 0 otherwise we'd be dividing by 0 below
-        if len(news_output[crypto]['title']) > 0:
+# define what to pair each coin to
+# AVOID PAIRING WITH ONE OF THE COINS USED IN KEYWORDS
+PAIRING = 'USDT'
 
-            news_output[crypto]['sentiment']['pos'] = len(news_output[crypto]['sentiment']['pos'])*100/len(news_output[crypto]['title'])
-            news_output[crypto]['sentiment']['mid'] = len(news_output[crypto]['sentiment']['mid'])*100/len(news_output[crypto]['title'])
-            news_output[crypto]['sentiment']['neg'] = len(news_output[crypto]['sentiment']['neg'])*100/len(news_output[crypto]['title'])
+# define how positive the news should be in order to place a trade
+# the number is a compound of neg, neu and pos values from the nltk analysis
+# input a number between -1 and 1
+SENTIMENT_THRESHOLD = 0.5
 
-            #print the output  for each coin to verify the result
-            print(crypto, news_output[crypto]['sentiment'])
+# define the minimum number of articles that need to be analysed in order
+# for the sentiment analysis to qualify for a trade signal
+# avoid using 1 as that's not representative of the overall sentiment
+MINUMUM_ARTICLES = 10
 
-    return news_output
+# define how often to run the code (check for new + try to place trades)
+# in minutes
+REPEAT_EVERY = 60
 
-# TO DO - If integrated with an exhange, add weight logic to avid placing a trade
-# when only 1 article is found and the sentiment is positive.
 
-#call the function
-#Delete Call if adding trading logic. Assign the function to a variable instead.
-calc_sentiment()
+############################################
+#        END OF USER INPUT VARIABLES       #
+#             Edit with care               #
+############################################
+
+
+
+
+# current price of CRYPTO pulled through the websocket
+CURRENT_PRICE = {}
+
+
+def ticker_socket(msg):
+    '''Open a stream for financial information for CRYPTO'''
+    if msg['e'] != 'error':
+        global CURRENT_PRICE
+        CURRENT_PRICE['{0}'.format(msg['s'])] = msg['c']
+    else:
+        print('error')
+
+
+# connect to the websocket client and start the socket
+bsm = BinanceSocketManager(client)
+for coin in keywords:
+    conn_key = bsm.start_symbol_ticker_socket(coin+PAIRING, ticker_socket)
+bsm.start()
+
+
+def calculate_volume():
+    '''Calculate the amount of CRYPTO to trade in USDT'''
+
+    while CURRENT_PRICE == {}:
+        print('Connecting to the socket...')
+        time.sleep(3)
+    else:
+        volume = {}
+        for coin in CURRENT_PRICE:
+            volume[coin] = float(QUANTITY / float(CURRENT_PRICE[coin]))
+            volume[coin] = float('{:.6f}'.format(volume[coin]))
+
+        return volume
+
+
+# load the csv file containg top 100 crypto feeds
+# want to scan other websites?
+# Simply add the RSS Feed url to the Crypto feeds.csv file
+with open('Crypto feeds.csv') as csv_file:
+
+    # open the file
+    csv_reader = csv.reader(csv_file)
+
+    # remove any headers
+    next(csv_reader, None)
+
+    # create empty list
+    feeds = []
+
+    # add each row cotaining RSS url to feeds list
+    for row in csv_reader:
+        feeds.append(row[0])
+
+
+# TO DO - run this in multiple processes to speed up the scraping
+def get_headlines():
+    '''Returns the last headline for each link in the CSV file'''
+
+    # add headers to the request for ElementTree. Parsing issues occur without headers
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0'
+    }
+
+    headlines = {'source': [], 'title': [], 'pubDate' : [] }
+
+    for feed in feeds:
+        try:
+            # grab the XML for each RSS feed
+            r = requests.get(feed, headers=headers, timeout=7)
+
+            # define the root for our parsing
+            root = ET.fromstring(r.text)
+
+            # identify the last Headline
+            channel = root.find('channel/item/title').text
+            pubDate = root.find('channel/item/pubDate').text
+
+            # append the source and the title
+            headlines['source'].append(feed)
+
+            # append the publication date
+            headlines['pubDate'].append(pubDate)
+
+
+            # some jank to ensure no alien characters are being passed
+            headlines['title'].append(channel.encode('UTF-8').decode('UTF-8'))
+            print(channel)
+        except:
+            print(f'Could not parse {feed}')
+
+    return headlines
+
+
+def categorise_headlines():
+    '''arrange all headlines scaped in a dictionary matching the coin's name'''
+    # get the headlines
+    headlines = get_headlines()
+    categorised_headlines = {}
+
+    # this loop will create a dictionary for each keyword defined
+    for keyword in keywords:
+        categorised_headlines['{0}'.format(keyword)] = []
+
+    # keyword needs to be a loop in order to be able to append headline to the correct dictionary
+    for keyword in keywords:
+
+        # looping through each headline is required as well
+        for headline in headlines['title']:
+
+            # appends the headline containing the keyword to the correct dictionary
+            if any(key in headline for key in keywords[keyword]):
+                categorised_headlines[keyword].append(headline)
+
+    return categorised_headlines
+
+
+def analyse_headlines():
+    '''Analyse categorised headlines and return NLP scores'''
+    sia = SentimentIntensityAnalyzer()
+    categorised_headlines = categorise_headlines()
+
+    sentiment = {}
+
+    for coin in categorised_headlines:
+        if len(categorised_headlines[coin]) > 0:
+            # create dict for each coin
+            sentiment['{0}'.format(coin)] = []
+            # append sentiment to dict
+            for title in categorised_headlines[coin]:
+                sentiment[coin].append(sia.polarity_scores(title))
+
+    return sentiment
+
+
+def compile_sentiment():
+    '''Arranges every compound value into a list for each coin'''
+    sentiment = analyse_headlines()
+    compiled_sentiment = {}
+
+    for coin in sentiment:
+        compiled_sentiment[coin] = []
+
+        for item in sentiment[coin]:
+            # append each compound value to each coin's dict
+            compiled_sentiment[coin].append(sentiment[coin][sentiment[coin].index(item)]['compound'])
+
+    return compiled_sentiment
+
+def compound_average():
+    '''Calculates and returns the average compoud sentiment for each coin'''
+    compiled_sentiment = compile_sentiment()
+    headlines_analysed = {}
+
+    for coin in compiled_sentiment:
+        headlines_analysed[coin] = len(compiled_sentiment[coin])
+
+        # calculate the average using numpy if there is more than 1 element in list
+        compiled_sentiment[coin] = np.array(compiled_sentiment[coin])
+
+        # get the mean
+        compiled_sentiment[coin] = np.mean(compiled_sentiment[coin])
+
+        # convert to scalar
+        compiled_sentiment[coin] = compiled_sentiment[coin].item()
+
+    return compiled_sentiment, headlines_analysed
+
+
+def buy():
+    '''Check if the sentiment is positive and keyword is found for each handle'''
+    compiled_sentiment, headlines_analysed = compound_average()
+    volume = calculate_volume()
+
+    for coin in compiled_sentiment:
+
+        # check if the sentiment and number of articles are over the given threshold
+        if compiled_sentiment[coin] > SENTIMENT_THRESHOLD and headlines_analysed[coin] > MINUMUM_ARTICLES:
+
+            # check the volume looks correct
+            print(f'preparing to buy {coin} with {volume} USDT at {CURRENT_PRICE[coin+PAIRING]}')
+
+            # create test order before pushing an actual order
+            test_order = client.create_test_order(symbol=coin+PAIRING, side='BUY', type='MARKET', quantity=volume[coin+PAIRING])
+
+            # try to create a real order if the test orders did not raise an exception
+            try:
+                buy_limit = client.create_order(
+                    symbol=coin+PAIRING,
+                    side='BUY',
+                    type='MARKET',
+                    quantity=volume[coin+PAIRING]
+                )
+
+            #error handling here in case position cannot be placed
+            except BinanceAPIException as e:
+                print(e)
+
+            except BinanceOrderException as e:
+                print(e)
+
+            # run the else block if the position has been placed and return some info
+            else:
+                # retrieve the last order
+                order = client.get_all_orders(symbol=coin+PAIRING, limit=1)
+
+                # convert order timsestamp into UTC format
+                time = order[0]['time'] / 1000
+                utc_time = datetime.fromtimestamp(time)
+
+                # grab the price of CRYPTO the order was placed at for reporting
+                bought_at = CURRENT_PRICE[coin+PAIRING]
+
+                # print order condirmation to the console
+                print(f"order {order[0]['orderId']} has been placed on {coin} with {order[0]['origQty']} at {utc_time} and bought at {bought_at}")
+
+                return bought_at, order
+
+        else:
+            print(f'Sentiment not positive enough for {coin}, or not enough headlines analysed: {compiled_sentiment[coin]}, {headlines_analysed[coin]}')
+
+
+
+if __name__ == '__main__':
+    print('Press Ctrl-Q to stop the script')
+    for i in count():
+        buy()
+        print(f'Iteration {i}')
+        time.sleep(60 * REPEAT_EVERY)
